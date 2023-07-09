9ab05c866421239f59170c79b9e7f0c5cde9dddc
maruf1996 <maruf3813@gmail.com>
2023-07-09 13:22:00 +0600

    'third-commit'

--- module-5-5/src/main.ts
+++ module-5-5/src/main.ts
@@ -7,4 +7,5 @@ const add = (param1: number, param2: number, param3: number): number => {
 
 const resetAddTwo = Methods.add(4, 6);
 const res = average(4, 6);
+console.log(resetAddTwo);
 // const resetAddTwo = addTwo(4, 6);
